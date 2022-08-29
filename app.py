c50d413d5c6736833766218367b18ff0b10663f8
lolka0d <alalaparad@gmail.com>
2022-08-29 22:09:59 +0300

    ddasdssad

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run()
